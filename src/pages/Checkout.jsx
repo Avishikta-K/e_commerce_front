9e9ec6071a446e7168b586a25da55f4c7fb80ca0
Avishikta <akgamingakg3@gmail.com>
2026-01-24 10:29:25 +0530

    Initial admin url changings

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -96,7 +96,7 @@ const Checkout = () => {
     };
 
     try {
-        const response = await fetch('http://localhost:5000/api/orders', {
+        const response = await fetch('https://fashion-store-ak.onrender.com/api/orders', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
